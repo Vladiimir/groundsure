e53d04b9b9d23dda9f2da696e73718f03c02d91d
Uladzimir_Davidchyk <vova-1985@tut.by>
2013-11-12 18:03:23 +0300

    break points

--- sites/all/themes/groundsure/js/script.js
+++ sites/all/themes/groundsure/js/script.js
@@ -14,12 +14,12 @@
 
     //----------------footer menu breakpoint----------------------------
         if($(window).width() < 974) $( "#header").find(".menu-level-1" ).clone().prependTo( "#footer" );
-        if($(window).width() < 767) $( "#footer").find(".menu-level-1" ).prepend( "<h3>Further Actions</h3>" );
+        if($(window).width() < 751) $( "#footer").find(".menu-level-1" ).prepend( "<h3>Further Actions</h3>" );
         $( window ).resize(function() {
             if($(this).width() > 973){
                 $( "#footer .menu-level-1" ).remove();
             }
-            else if($(this).width() > 767){
+            else if($(this).width() > 751){
                 $( "#footer .menu-level-1" ).remove();
                 $( "#header").find(".menu-level-1" ).clone().prependTo( "#footer" );
             }
