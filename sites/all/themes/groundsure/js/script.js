c75edbf41e5f9f6cd0bd57212519ec380e55b96c
Uladzimir_Davidchyk <vova-1985@tut.by>
2013-11-13 18:53:28 +0300

    ending breakpoints

--- sites/all/themes/groundsure/js/script.js
+++ sites/all/themes/groundsure/js/script.js
@@ -13,22 +13,29 @@
     //----------------end info toolkit----------------------------
 
     //----------------footer menu breakpoint----------------------------
-        if($(window).width() < 974) $( "#header").find(".menu-level-1" ).clone().prependTo( "#footer" );
-        if($(window).width() < 751) $( "#footer").find(".menu-level-1" ).prepend( "<h3>Further Actions</h3>" );
+        if($(window).width() < 974) $( "#header").find(".menu-block-2" ).clone().prependTo( "#footer" );
+        if($(window).width() < 751) $( "#footer").find(".menu-block-2" ).prepend( "<h3>Further Actions</h3>" );
         $( window ).resize(function() {
             if($(this).width() > 973){
                 $( "#footer .menu-level-1" ).remove();
             }
             else if($(this).width() > 751){
                 $( "#footer .menu-level-1" ).remove();
-                $( "#header").find(".menu-level-1" ).clone().prependTo( "#footer" );
+                $( "#header").find(".menu-block-2" ).clone().prependTo( "#footer" );
             }
             else {
                 $( "#footer .menu-level-1" ).remove();
-                $( "#header").find(".menu-level-1" ).clone().prependTo( "#footer" );
-                $( "#footer").find(".menu-level-1" ).prepend( "<h3>Further Actions</h3>" );
+                $( "#header").find(".menu-block-2" ).clone().prependTo( "#footer" );
+                $( "#footer").find(".menu-block-2" ).prepend( "<h3>Further Actions</h3>" );
             }
         });
     //----------------end footer menu breakpoint----------------------------
+
+    //----------------mobile-menu----------------------------
+        $( ".menu-block-3 > .menu > li.first > a").clone().prependTo( ".menu-block-3" );
+        $( ".menu-block-3" ).click(function() {
+            $( this ).find("> .menu" ).slideToggle();
+        });
+    //----------------end mobile-menu----------------------------
     });
 })(jQuery);
